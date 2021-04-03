d45db884490a3f9172cfc720e965f4f8f91ca5c3
firatmikyaz <66138781+firatmikyaz@users.noreply.github.com>
2021-04-03 11:14:11 +0300

    Ders paralelinde proje deEntityFramework yapısı oluşturuldu.Yeni nesneler eklendi ve buna bağlı diğer kamanlarda kod iyileştirilmesi sağlandı.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -18,12 +18,36 @@ namespace Business.Concrete
 
         public void Add(Car car)
         {
+            if (car.CarName.Length<2 & car.DailyPrice<0)
+            {
+                Console.WriteLine("Araba ismi iki harften az ve fiyatı sıfırdan büyük olmalıdır.");
+            }
             _carDal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
         }
 
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,24 +1,50 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleUI
 {
     class Program
     {
+        //static CarManager carManager = new CarManager(new InMemoryCarDal());
+
+        static CarManager carManager = new CarManager(new EfCarDal());
+        #region dummybrands codes
+        //var brands = new List<Brand> {
+        //    new Brand {Id =1, BrandName="Maserati" },
+        //    new Brand {Id =2, BrandName="Audi" },
+        //    new Brand {Id =3, BrandName="Benz" }
+        //}; 
+        #endregion
+
         static void Main(string[] args)
         {
-            GetAllTest();
+            //GetAllTest();
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            var cars = carManager.GetAll();
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.CarName);
+            }
+
+            Console.ReadKey();
+
+
         }
 
         private static void GetAllTest()
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.ModelYear + " - " + car.Description);
+
             }
+            Console.ReadKey();
         }
     }
 }
